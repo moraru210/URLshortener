68e7b698fbcf1146430602d5999a573847d10799
moraru210 <alexandru210moraru@gmail.com>
2022-07-25 00:15:31 +0100

    Introduce the POST and GET request handlers in handlers.go

--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"URLshortener/src/handler"
+	"URLshortener/src/store"
 	"fmt"
 	"github.com/gin-gonic/gin"
 )
@@ -9,9 +11,20 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hey Go URL Shortener !",
+			"message": "Welcome to the URL Shortener API",
 		})
 	})
+
+	r.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	// Note that store initialization happens here
+	store.InitializeStore()
 
 	err := r.Run(":9808")
 	if err != nil {
